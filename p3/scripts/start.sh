f692e96da39d05982757eb0b82c9d70c512f4eef
Stan Krivtsoff <stan.krivtsov@gmail.com>
2023-05-25 14:47:00 +0200

    new adds to start.sh

--- p3/scripts/start.sh
+++ p3/scripts/start.sh
@@ -3,7 +3,7 @@
 
 ##Let’s setup our cluster with 2 worker nodes (–agents in k3d command line) and expose the HTTP load balancer on the host on port 8080 (so that we can interact with our application)
 echo -e "\e[32m\nCreating cluster:\e[0m"
-k3d cluster create my-cluster --api-port 6443 -p 8080:80@loadbalancer --agents 2
+k3d cluster create my-cluster --api-port 6443 -p 8080:80@loadbalancer --agents 2 --wait
 
 echo -e "\e[32m\nCreating namespaces:\e[0m"
 kubectl create namespace argocd
@@ -16,8 +16,12 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 ## Use the watch command to ensure the pods are running and ready.
 kubectl wait --for=condition=Ready pods --all -n argocd
 
-
-
+echo -e "\e[32m\nApplying new password to ArgoCD:\e[0m"
+sudo kubectl -n argocd patch secret argocd-secret \
+  -p '{"stringData": {
+    "admin.password": "$2a$12$Iy3op7PDM/PWJ/8kRyn0ROWImQa5TSzQmL.8GTj0dNpEagZbQsnPG",
+    "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+  }}'
 
 ## We want our work inside ArgoCD to be hosted in a dedicated project named iot-project. 
 ## Allow only in-cluster deployments in the dev namespace and only for the acaren github repositories.
@@ -25,11 +29,23 @@ echo -e "\e[32m\nApplying config 'project.yaml':\e[0m"
 kubectl apply -f ../confs/project.yaml -n argocd
 
 ## Next, we will create an ArgoCD Application which will synchronize our Kubernetes manifests
-## hosted in the app folder on our github repository feature branch featurebranch_1 with the associated resources inside the dev namespace on our local cluster:
+## hosted in the app folder on our github repository (https://github.com/jesuisstan/acaren_iot_p3.git)
+## with the associated resources inside the dev namespace on our local cluster:
 echo -e "\e[32m\nApplying config 'application.yaml':\e[0m"
 kubectl apply -f ../confs/application.yaml -n argocd
 
+echo -e "\e[32m\nAwaiting pods are running and ready...\e[0m"
+sudo kubectl wait --for=condition=Ready pods --all -n argocd
 
-## to get access to ArgoCD’s web interface, we need to expose the argocd-server service on local machine:
-echo -e "\e[32m\nExposing the argocd-server service on our local machine:\e[0m"
-kubectl port-forward svc/argocd-server -n argocd 8888:443
+
+echo -e "\e[33m\nARGO CD IS  DEPLOYED!\nRUN THE THE FOLLOWING CMD TO WORK WITH IT:\n
+1) to access the dashboard:\n
+sudo kubectl port-forward svc/argocd-server --address 10.11.1.253 -n argocd 8081:80 2>&1 >/dev/null &\n
+\n
+2) to access wil's app:\n
+sudo kubectl port-forward svc/wil-playground -n dev 8888:8888 2>&1 >/dev/null &
+\e[0m"
+
+### to get access to ArgoCD’s web interface, we need to expose the argocd-server service on local machine:
+#echo -e "\e[32m\nExposing the argocd-server service on our local machine:\e[0m"
+#kubectl port-forward svc/argocd-server -n argocd 8888:443
